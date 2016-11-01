cdd3eacf53d67a46281b2be1e3a5595cdd21a4b2
Johan Andersson <900304@gmail.com>
2016-11-01 14:25:37 +0100

    First commit

---
+++ includes/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,20 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Hajtek", "text": "Ett svenskt gaming community", "tags": "hajtek, gaming, start, hem, startsida", "url": "http://www.hajtek.se"},
+     {"title": "Nyheter", "text": "Senaste nyheterna", "tags": "hajtek, gaming, nyheter, senaste, nytt", "url": "http://www.hajtek.se/news.html"},
+     {"title": "Sök", "text": "Sök på Hajtek", "tags": "hajtek, gaming, sök, sökfunktion", "url": "http://www.hajtek.se/search.html"},
+     {"title": "Servrar", "text": "Hajteks servrar", "tags": "hajtek, gaming, servrar, servers, minecraft, mc, ftb, feed, the, beast, ts, ts3, teamspeak", "url": "http://www.hajtek.se/servers.html"},
+     {"title": "Teamspeak 3", "text": "Hajteks teamspeak", "tags": "hajtek, gaming, teamspeak, ts, ts3, servrar, servers", "url": "http://www.hajtek.se/teamspeak.html"},
+     {"title": "Minecraft", "text": "Hajteks Minecraft-server", "tags": "hajtek, gaming, minecraft, mc, ftb, feed, the, beast, servrar, servers", "url": "http://www.hajtek.se/minecraft.html"},
+     {"title": "Streams", "text": "Hajteks streamers", "tags": "hajtek, gaming, twitch, stream, streams, draapparn, missfaebulous, brocahontaz, live", "url": "http://www.hajtek.se/streams.html"},
+     {"title": "Draapparn", "text": "Draapparn Gaming", "tags": "hajtek, gaming, twitch, stream, streams, draapparn, live", "url": "http://www.hajtek.se/draapparn.html"},
+     {"title": "MissFaebulous", "text": "", "tags": "hajtek, gaming, twitch, stream, streams, missfaebulous, live", "url": "http://www.hajtek.se/missfaebulous.html"},
+     {"title": "Brocahontaz", "text": "", "tags": "hajtek, gaming, twitch, stream, streams, brocahontaz, live", "url": "http://www.hajtek.se/missfaebulous.html"},
+     {"title": "Om Hajtek", "text": "Information om Hajtek", "tags": "hajtek, gaming, info, information, om, about", "url": "http://www.hajtek.se/about.html"},
+     {"title": "Facebook", "text": "Hajtek på Facebook", "tags": "hajtek, gaming, info, information, om, about, facebook, fb, social, media", "url": "https://sv-se.facebook.com/hajtek.se"},
+     {"title": "Twitch", "text": "Hajtek på Twitch", "tags": "hajtek, gaming, info, information, om, about, twitch, stream, social, media", "url": "https://www.twitch.tv/hajtektv"},
+     {"title": "Twitter", "text": "Hajtek på Twitter", "tags": "hajtek, gaming, info, information, om, about, twitter, tweet, social, media", "url": "https://twitter.com/HajtekSE"},
+]};
+
+
+
